79bc08f2e226a56931534874252d3c5502bbd958
Anish Gupta <anish-gupta@outlook.com>
2017-07-10 15:13:01 +0100

    Signed-off-by: Anish Gupta <anish-gupta@outlook.com>

--- pilhouette/send.py
+++ pilhouette/send.py
@@ -34,8 +34,8 @@ class Send(tk.Frame):
 
         self.entry_email = ttk.Entry(self)
         self.entry_email.grid(row=1, column=2, sticky="news")
-        self.entry_email.bind("<FocusIn>", lambda:self.keyboard = subprocess.Popen("matchbox-keyboard"))
-        self.entry_email.bind("<FocusOut>", lambda:self.keyboard.terminate() )
+        self.entry_email.bind("<FocusIn>", self.keyboard_open)
+        self.entry_email.bind("<FocusOut>", self.keyboard_close )
         #TODO: Some kinda event for popping up onscreen keyboard
 
         self.button_send = ttk.Button(self, text="Send", command=self.send)
@@ -55,6 +55,10 @@ class Send(tk.Frame):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
+    def keyboard_open(self):
+        self.keyboard = subprocess.Popen("matchbox-keyboard")
+    def keyboard_close(self):
+        self.keyboard.terminate()
     def send(self):
         self.label_status.config(text="Sending...")
         address = self.entry_email.get()
